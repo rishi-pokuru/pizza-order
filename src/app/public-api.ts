9c47443d37badcd9dabc22720f76507540a5e28e
Gopi Rama Krishna Yenugula <gopiramakrishna.y@quantela.com>
2023-08-28 11:41:19 +0530

    added public-api.ts file

---
+++ src/app/public-api.ts
@@ -0,0 +1,9 @@
+export * from './app.module';
+export * from './app.component';
+export * from './app-routing.module';
+
+export * from './pizza-main-layout/pizza-main-layout.component';
+export * from './pizza-header/pizza-header.component';
+export * from './pizza-detail/pizza-detail.component';
+
+export * from './pizza.service';
